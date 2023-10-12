6c40fe7e3677d3bcd1934b9f8ae0f010557371ae
biao <b10902133@ntu.edu.tw>
2023-10-12 10:06:51 +0800

    finish 1.1

--- hack1/frontend/src/routes/auth/Layout.tsx
+++ hack1/frontend/src/routes/auth/Layout.tsx
@@ -90,8 +90,8 @@ const AuthLayout = () => {
             {/* The logo should be vscoddit.svg in the public folder. */}
             {/* The logo should have alt text "VSCoddit Logo". */}
             {/* The title should be "VSCoddit" */}
-            <img data-testid="header-logo" className="h-5 w-5 brightness-200" />
-            <span data-testid="header-title" />
+            <img data-testid="header-logo" className="h-5 w-5 brightness-200" src="/vscoddit.svg" alt="VSCoddit Logo"/>
+            <span data-testid="header-title">VSCoddit</span>
             {/* END of TODO 1.1 */}
           </CardTitle>
           <CardDescription>
